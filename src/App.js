4ba0f2c4b105c17f0b48206447d6f5852cd9091d
Maria <mfb171201@gmail.com>
2021-09-02 13:07:32 -0400

    atividade realizada

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState } from 'react';
+
+function App() {
+  const [number, setNumber] = useState(Math.floor(Math.random() * 100) + 1)
+  function randomNumber(){
+    setNumber(Math.floor(Math.random() * 100) + 1)
+  }
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>{number}</p>
+        <button onClick={randomNumber}>Criar novo número</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
